c4c4edf5807f1e7c60139561951122e88d3dd92f
Digital Sparks B.V. <bas.van.straaten@digital-sparks.com>
2026-01-29 20:58:02 +0100

    update call

--- dist/profile.js
+++ dist/profile.js
@@ -713,6 +713,9 @@
         state.providerId = providerProfile.provider_id;
         providerProfileCache = providerProfile;
 
+        // Update price iframe URL now that we have the provider_id
+        updatePriceIframeUrl();
+
         renderProviderProfile(providerProfile);
 
         return providerProfile;
@@ -1467,6 +1470,5 @@
     init();
   }
 
-  window.onload = function () {
-    updatePriceIframeUrl();
-  };
+  // updatePriceIframeUrl is now called after fetchProviderProfile completes
+  // to avoid race condition where state.providerId is not yet set
